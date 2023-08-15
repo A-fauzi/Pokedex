c57e42b4578230b6756f821a5bac35bcfae974e1
A-fauzi <akhmadfauzi0101@gmail.com>
2023-08-15 16:57:01 +0700

    init commit

---
+++ app/src/main/java/com/afauzi/pokedex/presentation/view/main/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.afauzi.pokedex.presentation.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.afauzi.pokedex.R
+import com.afauzi.pokedex.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigation = binding.bottomNav
+        setupWithNavController(bottomNavigation, navController)
+    }
+}
