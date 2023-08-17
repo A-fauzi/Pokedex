9a9e977c1c7c4930a85c8ef84fe431bfe0111e4d
A-fauzi <akhmadfauzi0101@gmail.com>
2023-08-17 16:55:46 +0700

    create compare pokemon

--- app/src/main/java/com/afauzi/pokedex/presentation/view/main/fragment/ComparePokemon.kt
+++ app/src/main/java/com/afauzi/pokedex/presentation/view/main/fragment/ComparePokemon.kt
@@ -1,60 +1,143 @@
 package com.afauzi.pokedex.presentation.view.main.fragment
 
 import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.afauzi.pokedex.R
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.afauzi.pokedex.data.datasource.remote.PokeApiProvider
+import com.afauzi.pokedex.data.datasource.remote.PokeApiService
+import com.afauzi.pokedex.data.repository_implement.PokemonRepository
+import com.afauzi.pokedex.databinding.FragmentComparePokemonBinding
+import com.afauzi.pokedex.databinding.ItemComparisonPokeBinding
+import com.afauzi.pokedex.domain.entity.PokeDetail
+import com.afauzi.pokedex.presentation.adapter.AdapterSearchPokePaging
+import com.afauzi.pokedex.presentation.presenter.viewmodel.PokeViewModel
+import com.afauzi.pokedex.presentation.presenter.viewmodelfactory.PokeViewModelFactory
+import com.afauzi.pokedex.utils.Helpers
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ComparePokemon : Fragment(), AdapterSearchPokePaging.ListenerPokeAdapter {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ComparePokemon.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ComparePokemon : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentComparePokemonBinding
+    private lateinit var adapterSearchPokePaging: AdapterSearchPokePaging
+    private lateinit var pokeViewModel: PokeViewModel
+    private lateinit var pokeViewModelFactory: PokeViewModelFactory
+    private lateinit var pokeApiService: PokeApiService
+    private lateinit var pokemonRepository: PokemonRepository
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compare_pokemon, container, false)
+        binding = FragmentComparePokemonBinding.inflate(layoutInflater, container, false)
+        pokeApiService = PokeApiProvider.providePokeApiService()
+        pokemonRepository = PokemonRepository(pokeApiService)
+        adapterSearchPokePaging = AdapterSearchPokePaging(requireActivity(), this)
+        pokeViewModelFactory = PokeViewModelFactory(pokemonRepository, pokeApiService)
+        pokeViewModel = ViewModelProvider(this, pokeViewModelFactory)[PokeViewModel::class.java]
+        recyclerView = binding.rvPokemonLis
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ComparePokemon.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ComparePokemon().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpRecyclerView()
+
+        setUpViewModel()
+    }
+
+    private fun setUpViewModel() {
+        lifecycleScope.launch {
+            pokeViewModel.listDataPoke.collect { pagerData ->
+                adapterSearchPokePaging.submitData(pagerData)
+            }
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        binding.rvPokemonLis.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = adapterSearchPokePaging
+        }
+    }
+
+    private val listName = arrayListOf<String>()
+    private val listImage = arrayListOf<String>()
+
+    private val maxListSize = 2
+
+    override fun onClickListenerAdapter(name: String, image: String) {
+        if (listName.size < maxListSize) {
+            listName.add(name)
+            listImage.add(image)
+        }
+
+        if (listName.size == 1) {
+            binding.layoutCompareView.visibility = View.VISIBLE
+            binding.item1Poke.root.visibility = View.VISIBLE
+            binding.item1Poke.characterName.text = listName[0].capitalize()
+            binding.tvVs.visibility = View.VISIBLE
+            Glide.with(requireActivity())
+                .load(listImage[0])
+                .into(binding.item1Poke.itemImgPoke)
+        }
+
+        if (listName.size == 2) {
+            binding.item2Poke.root.visibility = View.VISIBLE
+            binding.item2Poke.characterName.text = listName[1].capitalize()
+            Glide.with(requireActivity())
+                .load(listImage[1])
+                .into(binding.item2Poke.itemImgPoke)
+
+            binding.btnCompare.visibility = View.VISIBLE
+            binding.rvPokemonLis.visibility = View.GONE
+        }
+
+
+        binding.btnCompare.setOnClickListener {
+            binding.layoutDataCompare.visibility = View.VISIBLE
+
+            Log.d("ComparePokemon", "lis: $listName")
+            Log.d("ComparePokemon", "data 1: ${listName[0]}")
+            Log.d("ComparePokemon", "data 2: ${listName[1]}")
+
+            pokeViewModel.pokeDetail.observe(viewLifecycleOwner) { pokeDetail ->
+                if (pokeDetail.name == listName[0]) {
+                    setDataToView(pokeDetail, binding.dataCompareItem1)
+                } else if (pokeDetail.name == listName[1]) {
+                    setDataToView(pokeDetail, binding.dataCompareItem2)
                 }
             }
+
+            fetchPokemonData()
+        }
+
+    }
+
+    private fun setDataToView(pokeDetail: PokeDetail, dataCompareView: ItemComparisonPokeBinding) {
+        dataCompareView.tvNamePokemon.text = pokeDetail.name
+        Glide.with(requireActivity())
+            .load(pokeDetail.sprites?.other?.home?.frontDefault)
+            .into(dataCompareView.ivPokemon)
+        dataCompareView.tvCodePokemon.text = Helpers.formatIdPoke(pokeDetail.id.toString())
+    }
+
+    private fun fetchPokemonData() {
+        lifecycleScope.launch {
+            listName.forEach { name ->
+                pokeViewModel.getPokeDetail(name)
+            }
+        }
     }
 }
